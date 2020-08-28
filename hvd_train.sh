c4f25b2d483d15d2ad91796c6a0e74e9933a0a46
ieggel <ivan.eggel@gmail.com>
2020-08-28 08:38:46 +0200

    uncomment unused stuff in hvd_train.sh for l2

--- hvd_train.sh
+++ hvd_train.sh
@@ -11,25 +11,6 @@ else
 fi
 
 
-
-# if [[ -d $FILE ]]
-# then
-#     read -r -p "override results in folder? [y/N] " response
-#     if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
-#     then
-#         horovodrun -np 2 \
-#         -H localhost:4 \
-#         python hvd_train_baseline.py 1001 $EXPERIMENT_TYPE
-#     else
-#         echo Stopping process to avoid overriding data.
-#     fi
-# else
-#     mkdir results/$EXPERIMENT_TYPE
-#     horovodrun -np 2 \
-#         -H localhost:4 \
-#         python hvd_train_baseline.py 1001 $EXPERIMENT_TYPE
-# fi
-
 if [[ -d $FILE ]]
 then
     read -r -p "override results in folder? [y/N] " response
